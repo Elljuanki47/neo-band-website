3b7e8447b46cb05f0783ea88fefe00ba583abb4f
Juan Ignacio Zarate <jizarate47@outlook.com>
2026-07-28 01:50:02 -0300

    feat: add hero component and band configuration

---
+++ src/config/band.config.ts
@@ -0,0 +1,20 @@
+export const bandConfig = {
+  name: "NEO",
+  
+  tagline: "Rock Alternativo",
+
+  description:
+    "Sitio oficial de NEO. Musica, shows y novedades",
+
+  social: {
+    instagram: "https://www.instagram.com/neo.bandaa",
+    spotify: "",
+    youtube: "",
+  },
+
+  release: {
+    title: "Amuleto",
+    cover: "/images/amuleto-cover.jpg",
+    spotifyEmbed: "",
+  },
+};
